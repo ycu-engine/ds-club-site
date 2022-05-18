ab80a5ad96094b725628eebb6c242b4d1205617e
YutaUra <yuuta3594@outlook.jp>
2022-05-18 18:54:45 +0900

    フロントエンドの土台を作成

---
+++ services/frontend/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <h1>About ページです</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ services/frontend/src/pages/users/ura.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header";
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+
+      <h1>うらのページです</h1>
+    </div>
+  );
+};
+
+export default Page;
