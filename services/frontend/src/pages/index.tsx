b88d7eece5836639f521cb189b71f5dacf323610
mokroke <pararerusupekku@icloud.com>
2022-05-21 23:10:37 +0900

    try first

--- services/frontend/src/pages/index.tsx
+++ services/frontend/src/pages/index.tsx
@@ -1,8 +1,5 @@
-import { Box, Heading } from "@chakra-ui/react";
-import { Body } from "../components/body";
 import { DefaultLayout } from "../components/DefaultLayout";
-import { Header } from "../components/Header";
-import { Sidebar, } from "../components/sidebar";
+
 
 const Page = () => {
   return (
---
+++ services/frontend/src/pages/operation2.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+
+const Page = () => {
+  return (
+    <Box>
+      <Header />
+
+      <Heading fontSize={"9xl"}>選択した</Heading>
+    </Box>
+  );
+};
+
+export default Page;
