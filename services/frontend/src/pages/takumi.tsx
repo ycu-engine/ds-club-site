f080a7c65a0d9512adfe4b3bc9b818fa16c0264c
nyatinte <takumi.062757@gmail.com>
2022-06-08 10:13:54 +0000

    初回のチュートリアルやったよ

---
+++ services/frontend/src/pages/takumi.tsx
@@ -0,0 +1,13 @@
+import { DefaultLayout } from '../components/DefaultLayout'
+
+const Page = () => {
+    return(
+        <div>
+            <DefaultLayout>
+                <h1>DefaultLayoutにテキストを追加してみる</h1>
+            </DefaultLayout>
+        </div>
+    )
+}
+
+export default Page
