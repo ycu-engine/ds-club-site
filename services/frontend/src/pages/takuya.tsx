7f896fb8fd683c57b55960670ec59ba61798f8f3
hapitaku <106806920+hapitaku@users.noreply.github.com>
2022-06-08 10:12:24 +0000

    初回のチュートリアルやったよ〜〜

---
+++ services/frontend/src/pages/takuya.tsx
@@ -0,0 +1,11 @@
+import {DefaultLayout} from '../components/DefaultLayout'
+
+const Page = () => {
+    return(
+        <DefaultLayout>
+             <h1>hello</h1>
+        </DefaultLayout>
+    )
+}
+
+export default Page
