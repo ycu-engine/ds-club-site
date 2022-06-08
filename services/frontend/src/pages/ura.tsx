fd8fccdb6b3dc092650aa980b882f7851a7c782c
YutaUra <yuuta3594@outlook.jp>
2022-06-08 10:12:23 +0000

    初回のチュートリアルやったよ

---
+++ services/frontend/src/pages/ura.tsx
@@ -0,0 +1,11 @@
+import { DefaultLayout } from '../components/DefaultLayout'
+
+const Page = () => {
+    return (
+        <DefaultLayout>
+            <h1>Hello Yuta.</h1>
+        </DefaultLayout>
+    )
+}
+
+export default Page
