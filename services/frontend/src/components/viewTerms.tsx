e2e845ceb53a9182135aeabf14af596909f94760
mokroke <pararerusupekku@icloud.com>
2022-07-01 11:07:04 +0000

    create viewTerms

---
+++ services/frontend/src/components/viewTerms.tsx
@@ -0,0 +1,15 @@
+import {
+    Text
+} from '@chakra-ui/react'
+import { DefaultLayout } from '../components/DefaultLayout'
+
+const Page = () => {
+  return (
+    <DefaultLayout >
+        <Text>あいうえお</Text>
+
+    <DefaultLayout />
+  )
+}
+
+export default Page
