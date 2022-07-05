5caa91a8e115903c121e4e6622db82a79005b866
nyatinte <takumi.062757@gmail.com>
2022-07-05 16:57:07 +0000

    responsive and high ux makeTestPage.tsx

--- services/frontend/src/pages/makeTestPage.tsx
+++ services/frontend/src/pages/makeTestPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import {
   Box,
   Heading,
@@ -14,10 +14,25 @@ import {
   Textarea,
   chakra,
   Tooltip,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  HStack,
+  Image,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from '@chakra-ui/react'
 
 import { DefaultLayout } from '../components/DefaultLayout'
 import { useState } from 'react'
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 
 type RadioChoicesProps = {
   correctIndex: string // 問題に対する正解の番号です。number型ではなくstring型であることに注意してください。
@@ -37,15 +52,15 @@ const RadioChoices = (props: RadioChoicesProps) => {
         {/* 選択肢の見た目はここで変更できる */}
         <Box
           {...getCheckboxProps()}
-          border={state.isChecked ? '4px solid blue' : ''}
-          w={200}
-          p={2}
+          border={state.isChecked ? '2px solid blue' : ''}
+          w="200px"
+          p="2"
           verticalAlign="middle"
-          rounded={20}
-          borderRadius={20}
+          rounded="20"
+          borderRadius={['10', '20']}
           bg="orange.400"
           h={['40px', '80px']}
-          _hover={{ transform: 'scale(1.1)', transitionDuration: '.5s' }}
+          _hover={{ bg: 'orange.300' }}
         >
           {/* 選択肢の文章 */}
           <Text {...getLabelProps()}>{text}</Text>
@@ -109,16 +124,22 @@ type TestProps = {
   radioText3: string
   radioText4: string
   correctIndex: string //　正解の選択肢の番号
+  image?: string //　画像ファイル
 }
 // テスト問題の全体のコンポーネント,  RadioChoicesを子コンポーネントとして使用
 const Test = (props: TestProps) => {
+  const {
+    testIndex,
+    testSentence,
+    radioText1,
+    radioText2,
+    radioText3,
+    radioText4,
+    image,
+    correctIndex,
+  } = props
   // radioTextはリスト形式にまとめておく
-  const radioTexts: string[] = [
-    props.radioText1,
-    props.radioText2,
-    props.radioText3,
-    props.radioText4,
-  ]
+  const radioTexts: string[] = [radioText1, radioText2, radioText3, radioText4]
   return (
     <Container>
       <VStack>
@@ -135,171 +156,228 @@ const Test = (props: TestProps) => {
         >
           {/* 問題番号 */}
           <Heading fontWeight="bold" p="5">
-            {`第${props.testIndex}問`}
+            {`第${testIndex}問`}
           </Heading>
           {/* 問題文 */}
-          <Text>{props.testSentence}</Text>
+          <Text>{testSentence}</Text>
+          <Image src={image} />
         </Box>
         {/* 解答部分 */}
-        <RadioChoices
-          correctIndex={props.correctIndex}
-          radioTexts={radioTexts}
-        />
+        <RadioChoices correctIndex={correctIndex} radioTexts={radioTexts} />
       </VStack>
     </Container>
   )
 }
 
+type AddTestMenuProps = {
+  AddTestFunc: React.Dispatch<React.SetStateAction<unknown>>
+  containerProps?: object
+}
+// 問題追加画面のコンポーネント
+const AddTestMenu = (props: AddTestMenuProps) => {
+  // 親からテスト追加用のコールバック関数を受け取る
+  const { AddTestFunc, containerProps } = props
+  //   入力された値を管理するHook
+  const [inputs, setInputs] = useState({
+    testIndex: '',
+    testSentence: '',
+    radioText1: '',
+    radioText2: '',
+    radioText3: '',
+    radioText4: '',
+    image: '',
+    correctIndex: '',
+  })
+  // 入力の状態を管理するHook
+  const [isFilled, setIsFilled] = useState<boolean>(false)
+
+  //   Inputに変更があったときの処理
+  const handleInputChange = (e) => {
+    // イベントを受け取る
+    const { value, name } = e.target
+    // Inputのnameに対応するstateを更新する
+    setInputs({ ...inputs, [name]: value })
+  }
+
+  // inputsの値が更新されるのを感知して実行する副作用フック
+  React.useEffect(() => {
+    console.log(inputs)
+    // 全て埋まっていたらtrueにする
+    setIsFilled(
+      Object.values({ ...inputs, image: '>_<' }).every((value) => value),
+    )
+  }, [inputs])
+
+  // 問題を追加する
+  const handleClickAddTestBtn = () => {
+    // 受け取ったコールバック関数を実行
+    AddTestFunc(inputs)
+  }
+
+  // inputに使用するPropsをまとめておく
+  const inputProps = {
+    outlineColor: 'black',
+    size: 'sm',
+    onChange: handleInputChange,
+  }
+  return (
+    // 縦に積んでいく
+    <VStack
+      bg="gray.100"
+      p="3"
+      spacing="3"
+      h={'100vh'}
+      overflowY="scroll"
+      {...containerProps}
+    >
+      <FormControl isRequired>
+        <FormLabel as="legend">問題番号</FormLabel>
+        <Input
+          {...inputProps}
+          type="number"
+          name="testIndex"
+          placeholder="問題番号を入力"
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel as="legend">問題文</FormLabel>
+        <Textarea
+          {...inputProps}
+          name="testSentence"
+          placeholder="問題文を入力"
+        />
+      </FormControl>
+      {/* 回答1 */}
+      {[1, 2, 3, 4].map((index) => {
+        return (
+          <FormControl isRequired key={`回答${index}`}>
+            <FormLabel as="legend">{`回答${index}`}</FormLabel>
+            <Input
+              {...inputProps}
+              name={`radioText${index}`}
+              placeholder={`回答${index}`}
+            />
+          </FormControl>
+        )
+      })}
+      {/* 正解の番号 */}
+      <FormControl as="fieldset">
+        <FormLabel as="legend">解答の番号</FormLabel>
+        <RadioGroup defaultValue="0">
+          <HStack>
+            <Radio value="1" name="correctIndex" onChange={handleInputChange}>
+              1
+            </Radio>
+            <Radio value="2" name="correctIndex" onChange={handleInputChange}>
+              2
+            </Radio>
+            <Radio value="3" name="correctIndex" onChange={handleInputChange}>
+              3
+            </Radio>
+            <Radio value="4" name="correctIndex" onChange={handleInputChange}>
+              4
+            </Radio>
+          </HStack>
+        </RadioGroup>
+      </FormControl>
+      {/* 写真を追加するボタン */}
+      <Input
+        placeholder="写真を追加"
+        type="file"
+        accept="image/*"
+        name="image"
+        onChange={handleInputChange}
+      />
+      {/* 問題を追加するボタン */}
+      <Tooltip
+        label={isFilled ? 'クリックして問題を追加' : '項目を全て埋めてください'}
+        shouldWrapChildren
+      >
+        <Button
+          display="block"
+          mx="auto"
+          isDisabled={!isFilled}
+          colorScheme="pink"
+          onClick={handleClickAddTestBtn}
+        >
+          追加する
+        </Button>
+      </Tooltip>
+    </VStack>
+  )
+}
+
+type AddTestMenuModalProps = {
+  children: ReactNode
+}
+const AddTestMenuModal = ({ children }: AddTestMenuModalProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button
+        colorScheme="teal"
+        mx="auto"
+        onClick={onOpen}
+        display={{ base: 'block', md: 'None' }}
+      >
+        問題を追加
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>問題追加画面</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{children}</ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              閉じる
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
 function makeTestPage() {
   // テスト用のデータを作成
-  const sampleData = {
+  const sampleData: TestProps = {
     testIndex: '5',
     testSentence: '顔が濡れるとダメなやつは誰？',
     radioText1: 'アンパンマン',
     radioText2: 'バイキンマン',
     radioText3: '食パンマン',
     radioText4: 'メロンパンナちゃん',
+    image: '',
     correctIndex: '1',
   }
   //   問題のデータを管理するHook
   const [testData, setTestData] = useState<TestProps[]>([sampleData])
-
-  // 問題追加画面のコンポーネント
-  const AddTestMenu = () => {
-    // オブジェクトでまとめておく
-    const inputAreaOptions = [
-      {
-        inputType: Input, //コンポーネントの指定, JSXElementを指定
-        category: '問題番号', // 何を追加するか
-        placeholder: '問題番号を入力', //　プレースホルダー
-        name: 'testIndex', //　識別用
-      },
-      {
-        inputType: Textarea,
-        category: '問題文',
-        placeholder: '問題文を入力',
-        name: 'testSentence',
-      },
-      {
-        inputType: Input,
-        category: '選択肢1',
-        placeholder: '選択肢1を入力',
-        name: 'radioText1',
-      },
-      {
-        inputType: Input,
-        category: '選択肢2',
-        placeholder: '選択肢2を入力',
-        name: 'radioText2',
-      },
-      {
-        inputType: Input,
-        category: '選択肢3',
-        placeholder: '選択肢3を入力',
-        name: 'radioText3',
-      },
-      {
-        inputType: Input,
-        category: '選択肢4',
-        placeholder: '選択肢4を入力',
-        name: 'radioText4',
-      },
-      {
-        inputType: Input,
-        category: '回答',
-        placeholder: '1~4の整数値を入力',
-        name: 'correctIndex',
-      },
-    ]
-
-    //   入力された値をstateで管理する
-    const [inputs, setInputs] = useState({
-      testIndex: '',
-      testSentence: '',
-      radioText1: '',
-      radioText2: '',
-      radioText3: '',
-      radioText4: '',
-      correctIndex: '',
-    })
-
-    //   Inputに変更があったときの処理
-    const handleInputChange = (e) => {
-      const target = e.target
-      // 値の更新
-      const value = target.value
-      const name = target.name
-      // Inputのnameに対応するstateも更新する
-      setInputs({ ...inputs, [name]: value })
-    }
-
-    const handleChagneTestData = () => {
-      // リストの後ろに追加する
-      setTestData([...testData, inputs])
-      console.log(testData)
-    }
-    // 全部終わったかどうか
-    let isFilled: boolean = Object.values(inputs).every((value) => value)
-
-    return (
-      // 縦に積んでいく
-      <VStack
-        bg="gray.200"
-        w="20%"
-        p="3"
-        spacing="3"
-        h={'100vh'}
-        overflowY="scroll"
-      >
-        {/* mapでinputAreaを作る */}
-        {inputAreaOptions.map((inputAreaOption) => {
-          return (
-            <Box>
-              <Text fontWeight="bold" py="2">
-                {inputAreaOption.category}
-              </Text>
-              <inputAreaOption.inputType
-                key={inputAreaOption.name}
-                name={inputAreaOption.name}
-                placeholder={inputAreaOption.placeholder}
-                outlineColor="black"
-                size="sm"
-                onChange={handleInputChange}
-              ></inputAreaOption.inputType>
-            </Box>
-          )
-        })}
-        {/* 写真を追加するボタン */}
-        <Button colorScheme="blue" variant="ghost">
-          写真を追加
-        </Button>
-        {/* 問題を追加するボタン */}
-        <Tooltip
-          label={
-            isFilled ? 'クリックして問題を追加' : '項目を全て埋めてください'
-          }
-          shouldWrapChildren
-        >
-          <Button
-            isDisabled={!isFilled}
-            colorScheme="pink"
-            onClick={handleChagneTestData}
-          >
-            追加する
-          </Button>
-        </Tooltip>
-      </VStack>
-    )
+  // データを追加する関数
+  const AddTestData = (inputs: TestProps): void => {
+    setTestData([...testData, inputs])
   }
-
   // makeTestPageのreturn
   return (
     <DefaultLayout>
       <Flex>
         {/* testDataはオブジェクトを要素にもつリストなのでテスト的に追加されたものを表示する */}
         <Test {...testData.slice(-1)[0]} />
-        <AddTestMenu />
+        <AddTestMenu
+          AddTestFunc={AddTestData}
+          containerProps={{ display: { base: 'None', md: 'initial' } }}
+        />
       </Flex>
+      <AddTestMenuModal>
+        <AddTestMenu
+          AddTestFunc={AddTestData}
+          containerProps={{
+            bg: 'transparent',
+            alignItems: 'center',
+          }}
+        />
+      </AddTestMenuModal>
     </DefaultLayout>
   )
 }
