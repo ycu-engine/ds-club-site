5b9182517a7f96b7b154288d82a43a8cc02aad58
nyatinte <takumi.062757@gmail.com>
2022-08-10 10:02:37 +0000

    ローディングページの仮作成

--- services/frontend/src/components/Authenticated.tsx
+++ services/frontend/src/components/Authenticated.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import type { ReactNode } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../clients/firebase'
+import { LoadingPage } from './LoadingPage'
 
 type AuthenticatedProps = {
   children: ReactNode
@@ -15,7 +16,7 @@ export const Authenticated = ({ children }: AuthenticatedProps) => {
   }
 
   if (loading) {
-    return <>Now Loading...</>
+    return <LoadingPage />
   }
 
   void router.push('/login')
---
+++ services/frontend/src/components/LoadingPage.tsx
@@ -0,0 +1,47 @@
+import { Text } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+import type { MotionStyle } from 'framer-motion'
+
+const MotionDivStyle: MotionStyle = {
+  alignItems: 'center',
+  background: '#EFF0F3',
+  bottom: 0,
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  left: 0,
+  position: 'fixed',
+  right: 0,
+  top: 0,
+}
+
+export const LoadingPage: React.FC = () => {
+  return (
+    <motion.div style={MotionDivStyle}>
+      <Text color="orange.500" fontSize="5xl">
+        Now Loading...
+      </Text>
+
+      <br />
+
+      <motion.div
+        animate={{
+          rotate: [0, 0, 270, 270, 0],
+          x: [-70, 70],
+        }}
+        style={{
+          background: '#E81B63',
+          borderRadius: '20%',
+          height: '60px',
+          width: '60px',
+        }}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+          flip: Infinity,
+        }}
+      />
+    </motion.div>
+  )
+}
