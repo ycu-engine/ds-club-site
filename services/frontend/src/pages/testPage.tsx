18b7a230b3971e38da98e17eea207d13eef7e7a6
YutaUra <yuuta3594@outlook.jp>
2022-08-10 07:33:21 +0000

    fix eslint

--- services/frontend/src/pages/testPage.tsx
+++ services/frontend/src/pages/testPage.tsx
@@ -1,5 +1,4 @@
 import { Flex, Text, Button, Heading, Checkbox } from '@chakra-ui/react'
-
 import React from 'react'
 import { DefaultTestLayout } from '../components/DefaultTestLayout'
 const Page = () => {
