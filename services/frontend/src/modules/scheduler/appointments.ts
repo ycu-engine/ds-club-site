3287aadd6a8d142b9ea79702f8b346f96f76d77b
nyatinte <takumi.062757@gmail.com>
2022-08-24 05:21:50 +0000

    スケジューラを作成

---
+++ services/frontend/src/modules/scheduler/appointments.ts
@@ -0,0 +1,89 @@
+// イベントの設定日時の範囲 (YYYY, MM, DD) MMは-1して補正
+const loopStartDate: Date = new Date(2022, 7 - 1, 20)
+const loopEndDate: Date = new Date(2022, 9 - 1, 20)
+
+//
+type DayAppointmentTemplate = {
+  title: string
+  startHourMinite: [number, number]
+  endHourMinite: [number, number]
+  location: string
+}
+// 火曜日のテンプレート
+const tuesdayAppointmentTemplate: DayAppointmentTemplate = {
+  endHourMinite: [19, 0],
+  location: 'zoom',
+  startHourMinite: [18, 0],
+  title: '週目標の設定',
+}
+// 金曜日のテンプレート
+const fridayAppointmentTemplate: DayAppointmentTemplate = {
+  endHourMinite: [20, 0],
+  location: '5号館305',
+  startHourMinite: [18, 0],
+  title: '勉強会',
+}
+// 予定の型
+type AppointmentType = {
+  title: string
+  startDate: Date
+  endDate: Date
+  location: string
+}
+// appointを作成する関数を定義
+type makeAppointmentType = (
+  date: Date,
+  dayTemplate: DayAppointmentTemplate,
+) => AppointmentType
+const makeAppointment: makeAppointmentType = (date, dayTemplate) => {
+  const [startHour, startMinite] = dayTemplate.startHourMinite
+  const [endHour, endMinite] = dayTemplate.endHourMinite
+  return {
+    endDate: new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      endHour,
+      endMinite,
+    ),
+    location: dayTemplate.location,
+    startDate: new Date(
+      date.getFullYear(),
+      date.getMonth(), //JSの仕様なので補正
+      date.getDate(),
+      startHour,
+      startMinite,
+    ),
+    title: dayTemplate.title,
+  }
+}
+const appointments: AppointmentType[] = []
+// 期間内でループしてイベントを設定していく
+let loopDate = new Date(loopStartDate)
+while (loopDate <= loopEndDate) {
+  // 1日進めてrecast
+  const newDate = loopDate.setDate(loopDate.getDate() + 1)
+  loopDate = new Date(newDate)
+  switch (loopDate.getDay()) {
+    // 火曜日
+    case 2:
+      appointments.push(makeAppointment(loopDate, tuesdayAppointmentTemplate))
+      break
+    // 金曜日
+    case 5:
+      appointments.push(makeAppointment(loopDate, fridayAppointmentTemplate))
+      break
+    default:
+      break
+  }
+}
+
+// 特別なイベントの設定
+const BBQEvent: AppointmentType = {
+  endDate: new Date(2022, 9 - 1, 10, 15, 0),
+  location: '海の公園',
+  startDate: new Date(2022, 9 - 1, 10, 10, 0),
+  title: 'BBQ',
+}
+appointments.push(BBQEvent)
+export default appointments
