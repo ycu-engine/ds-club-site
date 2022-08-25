5591ac1646b4996662340b800bd89a45da078157
YutaUra <yuuta3594@outlook.jp>
2022-08-25 15:30:03 +0000

    mui の設定

--- services/frontend/src/pages/sample.tsx
+++ services/frontend/src/pages/sample.tsx
@@ -1,10 +1,13 @@
 import { DefaultLayout } from '../components/DefaultLayout'
+import { ThemeProvider, createTheme } from '@mui/material'
 import { DSScheduler } from '../modules/scheduler/ds_scheduler'
 
 export default () => {
   return (
-    <DefaultLayout>
-      <DSScheduler />
-    </DefaultLayout>
+    <ThemeProvider theme={createTheme()}>
+      <DefaultLayout>
+        <DSScheduler />
+      </DefaultLayout>
+    </ThemeProvider>
   )
 }
