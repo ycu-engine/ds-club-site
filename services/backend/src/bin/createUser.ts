972c3bea418618470baeff289d96bfa6e530dbf4
NakamuraTakumi <104000239+nyatinte@users.noreply.github.com>
2022-09-04 20:09:07 +0900

    Update services/backend/src/bin/createUser.ts
    改行を削除

--- services/backend/src/bin/createUser.ts
+++ services/backend/src/bin/createUser.ts
@@ -3,7 +3,6 @@ import { createUserWithId } from '../modules/user'
 
 const main = async () => {
   await createUserWithId('LFoYLqtv4t5AXKSNDzuWC3ngTjlD', {
-
     currentRank: RankKind.Evangelist,
     name: '諸田健太朗',
     paymentStatus: PaymentStatus.Paid,
