e3c3559d8ec5b26d6abecd65232222226e1029a6
NakamuraTakumi <104000239+nyatinte@users.noreply.github.com>
2022-09-25 23:55:20 +0900

    Update services/backend/src/bin/createUserWithId.ts

--- services/backend/src/bin/createUserWithId.ts
+++ services/backend/src/bin/createUserWithId.ts
@@ -3,7 +3,7 @@ import { createUserWithId } from '../modules/user'
 const main = async () => {
   await createUserWithId('Vl25tBuup7Af38GD2Qx4cOj8g1MB', {
     email: 'hoge@fuga.com',
-    name: '中村拓実',
+    name: '認証用ユーザー',
   })
 }
 
