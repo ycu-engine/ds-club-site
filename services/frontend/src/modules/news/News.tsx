4ae862fefb506ecfc5f47153583985236467522d
nyatinte <takumi.062757@gmail.com>
2022-09-26 05:25:07 +0000

    ロード中やお知らせがない場合の処理を追加

--- services/frontend/src/modules/news/News.tsx
+++ services/frontend/src/modules/news/News.tsx
@@ -8,18 +8,16 @@ import {
   Container,
   TabPanel,
   Text,
+  Spinner,
+  BoxProps,
 } from '@chakra-ui/react'
+import type { ReactNode } from 'react'
 import { useNewsQuery } from '../../generates/graphql'
 
-export const News = () => {
-  const { data, loading, error } = useNewsQuery({
-    variables: {},
-  })
-  console.info(data, loading, error)
-  if (!data) {
-    return null
-  }
-
+type NewsProps = {
+  children?: ReactNode
+} & BoxProps
+const NewsWrapper = ({ children, ...props }: NewsProps) => {
   return (
     <Container py="5">
       <Tabs
@@ -32,40 +30,72 @@ export const News = () => {
       >
         <Heading fontSize={['xl', '2xl', '3xl']}>お知らせ</Heading>
 
-        <Box border="1px solid black" borderRadius="20" m={['2,', '5']}>
-          <TabPanels h="50vh" overflow="scroll" p="5">
-            {data.getNews.map((news) => {
-              console.info(news)
-              return (
-                <TabPanel key={news.title}>
-                  <Box>
-                    <Heading fontSize={['xl', '2xl', '3xl']}>
-                      {news.title}
-                    </Heading>
-
-                    <Text>{news.body}</Text>
-                  </Box>
-                </TabPanel>
-              )
-            })}
-          </TabPanels>
+        <Box
+          border="1px solid black"
+          borderRadius="20"
+          h="50vh"
+          m={['2,', '5']}
+          {...props}
+        >
+          {children}
         </Box>
-
-        <TabList justifyContent="center">
-          {data.getNews.map((news) => {
-            return (
-              <Tab
-                _focus={{ outline: 'none' }}
-                _selected={{ bg: 'blackAlpha.200', color: 'red' }}
-                key={news.title}
-              >
-                ・
-              </Tab>
-            )
-          })}
-        </TabList>
       </Tabs>
     </Container>
+  )
+}
+
+export const News = () => {
+  const { data, loading } = useNewsQuery({
+    variables: {},
+  })
+
+  console.info(data)
+  if (loading) {
+    return (
+      <NewsWrapper alignItems="center" display="flex">
+        <Spinner display="flex" mx="auto" />
+      </NewsWrapper>
+    )
+  }
+
+  if (!data || data.getNews.length === 0) {
+    return (
+      <NewsWrapper>
+        <Text>お知らせはありません</Text>
+      </NewsWrapper>
+    )
+  }
+  return (
+    <NewsWrapper>
+      <TabPanels h="50vh" overflow="scroll" p="5">
+        {data?.getNews.map((news) => {
+          console.info(news)
+          return (
+            <TabPanel key={news.title}>
+              <Box>
+                <Heading fontSize={['xl', '2xl', '3xl']}>{news.title}</Heading>
+
+                <Text>{news.body}</Text>
+              </Box>
+            </TabPanel>
+          )
+        })}
+      </TabPanels>
+
+      <TabList justifyContent="center">
+        {data.getNews.map((news) => {
+          return (
+            <Tab
+              _focus={{ outline: 'none' }}
+              _selected={{ bg: 'blackAlpha.200', color: 'red' }}
+              key={news.title}
+            >
+              ・
+            </Tab>
+          )
+        })}
+      </TabList>
+    </NewsWrapper>
   )
 }
 
