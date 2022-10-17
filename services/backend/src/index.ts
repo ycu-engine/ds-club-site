7ea234da679597b5d0cdae1f32eae375813a721f
YutaUra <yuuta3594@outlook.jp>
2022-10-17 15:05:14 +0000

    fix port

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -3,7 +3,7 @@ import { createServer } from './server'
 const bootstrap = async () => {
   const server = await createServer()
 
-  const { url } = await server.listen()
+  const { url } = await server.listen(process.env['PORT'] || 4000)
   console.info(`🚀 Server ready at ${url}`)
 }
 
