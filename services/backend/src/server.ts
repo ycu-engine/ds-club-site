01ef7cf90fa81e9abfbb112cbd41daf7793af2ef
nyatinte <takumi.062757@gmail.com>
2022-11-19 07:19:40 +0000

    サーバーは以前にもどず

--- services/backend/src/server.ts
+++ services/backend/src/server.ts
@@ -1,14 +1,13 @@
 import { ApolloServer } from 'apollo-server'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { createContext } from './context'
-import { createSchema, resolvers } from './schema'
+import { createSchema } from './schema'
 
 export const createServer = async () => {
   return new ApolloServer({
     context: createContext,
     debug: true,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-    resolvers,
     schema: await createSchema(),
   })
 }
