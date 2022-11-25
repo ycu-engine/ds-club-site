f002ed7c3ca48b93db7e1a85d7416d65f4198cca
nyatinte <takumi.062757@gmail.com>
2022-11-25 07:24:59 +0000

    API繋ぎ込み

--- services/frontend/src/modules/menter/menterManagement/MenterManagementPage.tsx
+++ services/frontend/src/modules/menter/menterManagement/MenterManagementPage.tsx
@@ -4,9 +4,13 @@ import {
   FormLabel,
   Flex,
   IconButton,
+  useToast,
 } from '@chakra-ui/react'
 
-import { useMenterManagementPageQuery } from 'generates/graphql'
+import {
+  useMenterManagementPageQuery,
+  useMenterManagementPage_CreateMenterGroupMutation,
+} from 'generates/graphql'
 import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -23,7 +27,33 @@ const schema = z.object({
 })
 type formValues = z.infer<typeof schema>
 export const MenterManagementPage = () => {
-  const { data, loading, error } = useMenterManagementPageQuery()
+  const toast = useToast()
+  const {
+    data,
+    loading: queryLoading,
+    error: queryError,
+  } = useMenterManagementPageQuery()
+  const [
+    menterManagementPageCreateMenterGroupMutation,
+    { loading: mutateLoading },
+  ] = useMenterManagementPage_CreateMenterGroupMutation({
+    onCompleted: () => {
+      toast({
+        duration: 5000,
+        isClosable: true,
+        status: 'success',
+        title: 'メンターを登録しました',
+      })
+    },
+    onError: (error) => {
+      toast({
+        duration: 5000,
+        isClosable: true,
+        status: 'error',
+        title: error.message,
+      })
+    },
+  })
   const { control, watch, handleSubmit } = useForm<formValues>({
     resolver: zodResolver(schema),
   })
@@ -34,10 +64,24 @@ export const MenterManagementPage = () => {
     control,
     name: 'mentee',
   })
-  const onSubmit: SubmitHandler<formValues> = (form) => {
-    console.info(form)
+  const onSubmit: SubmitHandler<formValues> = () => {
+    menterManagementPageCreateMenterGroupMutation({
+      variables: {
+        input: {
+          // 変換する
+          menteeIds: mentee.map((m) => {
+            const user =
+              data?.getRegularUsers.find((u) => u.name === m) ||
+              data?.getTrialUsers.find((u) => u.name === m)
+            return user?.id || ''
+          }),
+          menterId:
+            data?.getRegularUsers.find((u) => u.name === menter)?.id || '',
+        },
+      },
+    })
   }
-  if (error) {
+  if (queryError) {
     return <>エラーが発生しました</>
   }
   return (
@@ -46,7 +90,7 @@ export const MenterManagementPage = () => {
 
       <UserAutoComplete<formValues>
         control={control}
-        isDisabled={loading}
+        isDisabled={queryLoading}
         name="menter"
         users={data?.getRegularUsers}
       />
@@ -57,8 +101,9 @@ export const MenterManagementPage = () => {
         <Flex key={field.id} py="2">
           <UserAutoComplete<formValues>
             control={control}
-            isDisabled={loading}
+            isDisabled={queryLoading}
             name={`mentee[${index}].name`}
+            // 選択済みユーザーを除外する
             users={data?.getRegularUsers.filter(
               (user) => user.name !== menter && !mentee.includes(user.name),
             )}
@@ -81,7 +126,9 @@ export const MenterManagementPage = () => {
           onClick={() => append({ name: '' })}
         />
 
-        <Button onClick={handleSubmit(onSubmit)}>送信</Button>
+        <Button isLoading={mutateLoading} onClick={handleSubmit(onSubmit)}>
+          送信
+        </Button>
       </Flex>
     </Container>
   )
